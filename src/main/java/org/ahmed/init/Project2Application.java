a409d5ff7ba8f45b0c97603b20b33ecb5acec47f
Soyeb Ahmed <soyeb88@gmail.com>
2022-12-28 12:44:54 -0500

    war convert

--- src/main/java/org/ahmed/init/Project2Application.java
+++ src/main/java/org/ahmed/init/Project2Application.java
@@ -2,9 +2,10 @@ package org.ahmed.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Project2Application {
+public class Project2Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
