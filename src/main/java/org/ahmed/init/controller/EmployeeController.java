3d847f44f947db271ecaffd05ee7af4a45be7268
Soyeb Ahmed <soyeb88@gmail.com>
2023-01-14 16:23:43 -0500

    cross origin update

--- src/main/java/org/ahmed/init/controller/EmployeeController.java
+++ src/main/java/org/ahmed/init/controller/EmployeeController.java
@@ -21,7 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 //@CrossOrigin(origins="http://localhost:8080/")
-@CrossOrigin(origins="http://192.168.1.46/")
+//@CrossOrigin(origins="http://192.168.1.46/")
+@CrossOrigin(origins="http://98.15.45.185/")
+
 @RestController
 @RequestMapping(value="/api/v1/")
 public class EmployeeController {
